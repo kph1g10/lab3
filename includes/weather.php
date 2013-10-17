b660ff519acf60e24b6c4b5c7509ef08895b66c6
kph1g10 <kph1g10@ecs.soton.ac.uk>
2013-10-17 16:47:46 +0100

    Made weather data readable
    Made additions to CSS style for weather data.
    Picked out individual pieces of weather data and layed it out in such a
    way that it is readable to humans.

--- includes/weather.php
+++ includes/weather.php
@@ -1,5 +1,19 @@
 	<?php $data = json_decode(file_get_contents('http://comp2203.ecs.soton.ac.uk/labs/lab3/api/location.php?location=3772'));?>
 
 <div id="weather">
-	<?php print_r($data); ?>
+	<h3><?php print_r('DATA FOR: ' . $data->name . ', ' . $data->country); ?></h3>
+	<p> <?php print_r('Continent: ' . $data->continent); ?></p>
+	<p> <?php print_r('Latitude: ' . $data->position->lat); ?>&deg</p>
+	<p> <?php print_r('Longitude: ' . $data->position->long); ?>&deg</p>
+	<p> <?php print_r('Date & Time: ' . $data->time); ?></p>
+	<h4>WEATHER</h4>
+	<p> <?php print_r('Weather Type: ' . $data->weather->weatherType); ?></p>
+	<p> <?php print_r('Temp: ' . $data->weather->temperature); ?> &deg;C</p>
+	<p> <?php print_r('Pressure: ' . $data->weather->pressure . ' mb'); ?></p>
+	<p> <?php print_r('Visibility: ' . (($data->weather->visibility) / 1000) . ' km'); ?></p>
+	<h5>Wind</h5>
+	<p> <?php print_r('Direction (Compass): ' . $data->weather->wind->compass); ?></p>
+	<p> <?php print_r('Direction (Angle): ' . $data->weather->wind->angle); ?>&deg</p>
+	<p> <?php print_r('Speed: ' . $data->weather->wind->speed); ?> kph</p>
+	
 </div>

4433f087fca5147ae8ccad84125f7194fc7f6f35
kph1g10 <kph1g10@ecs.soton.ac.uk>
2013-10-17 15:31:15 +0100

    Footer changed to automatically change year
    Instead of stating 2013 as the copyright year, which would have to be
    changed every year, the code now uses php to call the current year.
    Because all of the web pages are already calling the footer.php content
    with an include function this will update all pages automatically.

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <div id="footer" class="row">
-	<p class="col-md-offset-1 text-muted">Designed by Boyd's Artistic Design Company &copy; 2013</p>
+	<p class="col-md-offset-1 text-muted">Designed by Boyd's Artistic Design Company &copy; <?php echo date('Y');?></p>
 </div>
